09f019725f6c85248065d09840bc3f002945b33c
DevMai90 <dmai90@yahoo.com>
2020-06-25 22:17:58 -0700

    Remove console log and unused variable

--- js/index.js
+++ js/index.js
@@ -1,10 +1,8 @@
 // Change navbar color on scroll
 const nav = document.querySelector('.nav');
-const home = document.querySelector('#home');
 
 window.onscroll = () => {
   const top = window.scrollY;
-  console.log(top);
   if (top >= home.offsetHeight * 0.6) nav.classList.add('nav--active');
   else nav.classList.remove('nav--active');
 };
