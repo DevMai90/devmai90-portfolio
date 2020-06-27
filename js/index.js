44d66a39c545923bbf4c81642192c6ede6c00245
DevMai90 <dmai90@yahoo.com>
2020-06-27 04:05:55 -0700

    Update UI when hovering or focusing over nav links

--- js/index.js
+++ js/index.js
@@ -1,8 +1,18 @@
 // Change navbar color on scroll
 const nav = document.querySelector('.nav');
+const navItems = document.querySelectorAll('.nav__link');
 
 window.onscroll = () => {
   const top = window.scrollY;
-  if (top >= home.offsetHeight * 0.6) nav.classList.add('nav--active');
-  else nav.classList.remove('nav--active');
+  if (top >= home.offsetHeight * 0.6) {
+    nav.classList.add('nav--active');
+    navItems.forEach((item) => {
+      item.classList.add('nav__link--active');
+    });
+  } else {
+    nav.classList.remove('nav--active');
+    navItems.forEach((item) => {
+      item.classList.remove('nav__link--active');
+    });
+  }
 };
